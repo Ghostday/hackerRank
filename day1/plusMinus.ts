0668b0130b0e8c3c9d42c34cad69d22a16104610
Chris <fablekey@gmail.com>
2022-07-24 15:59:09 +1000

    plusMinus done

---
+++ day1/plusMinus.ts
@@ -0,0 +1,25 @@
+const unfilteredArr = [1, 1, 2, 0, -2, 5, 100, 2, -5, -5, -2, 0, 0, 5, -10]
+function plusMinus(arr: number[]) {
+  const numOfEle = arr.length;
+  let [positives, negatives, neutrals] = [0, 0, 0];
+
+  for (let i = 0; i < numOfEle; i++) {
+    switch (Math.sign(arr[i])) {
+      case 1:
+        positives++
+        break;
+      case 0:
+        neutrals++
+        break;
+      case -1:
+        negatives++
+        break;
+    }
+  }
+
+  const getRatio = (num) => (num / numOfEle).toFixed(6)
+  console.log(getRatio(positives), getRatio(negatives), getRatio(neutrals))
+
+}
+
+plusMinus(unfilteredArr)
